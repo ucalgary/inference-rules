5983239e62cf42374bf7f5540a7196535b909d97
King Chung Huang <kinghuang@mac.com>
2014-05-04 19:21:15 -0600

    Add unit tests for predicates
    Create basic unit tests for creating and evaluating predicates.

---
+++ python/rules/tests/test_predicates.py
@@ -0,0 +1,46 @@
+import unittest
+
+from ..expressions import Expression
+from ..predicates import Predicate, ComparisonPredicate
+
+class PredicatesBasicCreationAndEvaluationTest(unittest.TestCase):
+
+	def setUp(self):
+		import random
+		import string
+
+		self.key = ''.join(random.choice(string.ascii_lowercase) for _ in range(6))
+		self.value = ''.join(random.choice(string.ascii_lowercase) for _ in range(6))
+		self.object = {
+			self.key: self.value
+		}
+
+	def testPredicateWithValue(self):
+		predicate = Predicate.predicateWithValue(True)
+		evaluatedValue = predicate.evaluateWithObject(self.object)
+		self.assertEqual(evaluatedValue, True)
+
+	def testComparisonPredicate(self):
+		leftExpression = Expression.expressionForKeyPath(self.key)
+		rightExpression = Expression.expressionForConstantValue(self.value)
+		predicate = ComparisonPredicate(leftExpression, rightExpression)
+		evaluatedValue = predicate.evaluateWithObject(self.object)
+		self.assertEqual(evaluatedValue, True)
+
+	def testAndPredicate(self):
+		leftExpression = Expression.expressionForKeyPath(self.key)
+		rightExpression = Expression.expressionForConstantValue(self.value)
+		comparisonPredicate = ComparisonPredicate(leftExpression, rightExpression)
+		predicate = Predicate.andPredicateWithSubpredicates([comparisonPredicate, comparisonPredicate])
+		evaluatedValue = predicate.evaluateWithObject(self.object)
+		self.assertEqual(evaluatedValue, True)
+
+	def testOrPredicate(self):
+		leftExpression = Expression.expressionForKeyPath(self.key)
+		correctRightExpression = Expression.expressionForConstantValue(self.value)
+		incorrectRightExpression = Expression.expressionForConstantValue(None)
+		correctPredicate = ComparisonPredicate(leftExpression, correctRightExpression)
+		incorrectPredicate = ComparisonPredicate(leftExpression, incorrectRightExpression)
+		predicate = Predicate.orPredicateWithSubpredicates([correctPredicate, incorrectPredicate])
+		evaluatedValue = predicate.evaluateWithObject(self.object)
+		self.assertEqual(evaluatedValue, True)
