46b3decbe68df97bb17af9199a3cf2f327905e09
King Chung Huang <kinghuang@mac.com>
2014-05-06 21:16:52 -0600

    Fix end of string clause in scanWithParseFunction
    Change the while clause to calculate if the end of the string has been
    reached based on loc and i. Using the atEnd property does not work
    because scanLocation is not being modified.

--- python/rules/scanners.py
+++ python/rules/scanners.py
@@ -139,7 +139,7 @@ class Scanner(object):
 		if self.atEnd:
 			return f
 			
-		while not self.atEnd:
+		while loc + i + 1 < len(string):
 			try:
 				f = func(string[loc:loc + i + 1])
 			except ValueError:
