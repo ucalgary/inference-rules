481ddcbcf0db5b3298bc2347eeb7889ee066e973
King Chung Huang <kinghuang@mac.com>
2014-05-08 11:48:55 -0600

    Correct arguments for Rule class
    Rules require three arguments:
    
    * a left-hand side specifier (Predicate) that specifies a condition
    that must be true for the rule to be a candidate to fire
    * a right-hand-side key (String) that matches a key to be inferred
    * a value (Expression) that specifies the value for the right-hand-side
    key when a rule fires
    
    In DirectToWeb’s Rule system, the right-hand-side is described in
    Assignment objects, which contain both the key and the value. In this
    system, the two parts of the right-hand-side are separated.

--- python/rules/rules.py
+++ python/rules/rules.py
@@ -6,22 +6,27 @@ DefaultModel = None
 
 class Rule(object):
 	
-	def __init__(self, lhs, rhs, weight=0):
-		if rhs is None:
-			raise ValueError('rhs cannot be None')
+	def __init__(self, specifier, key, value, weight=0):
+		if key is None:
+			raise ValueError('key cannot be None')
 
-		self._lhs = lhs 	# Condition (Predicate)
-		self._rhs = rhs 	# Action (Expression)
+		self._specifier = specifier # Condition (Predicate)
+		self._key = key             # Key (String)
+		self._value = value         # Value (Expression)
 		self._weight = weight
 		self._priority = -1
 
 	@property
-	def lhs(self):
-		return self._lhs
+	def specifier(self):
+		return self._specifier
 
 	@property
-	def rhs(self):
-		return self._rhs
+	def key(self):
+		return self._key
+
+	@property
+	def value(self):
+		return self._value
 
 	@property
 	def weight(self):
@@ -32,19 +37,19 @@ class Rule(object):
 		return 0
 
 	def canFireInContext(self, context):
-		return self.lhs.evaluateWithObject(context) if self.lhs not None else True
+		return self.specifier.evaluateWithObject(context) if self.specifier not None else True
 
 	def fire(self, context):
-		assert rhs is not None
-		return self.rhs.expressionValueWithObject(context)
+		assert self.value is not None
+		return self.value.expressionValueWithObject(context)
 
 	def priority(self):
 		if self._priority is not -1:
 			return self._priority
 
 		priority = self.weight
-		if isinstance(self.lhs, predicate.CompoundPredicate)
-			priority += len(self.lhs.subpredicates)
+		if isinstance(self.specifier, predicate.CompoundPredicate)
+			priority += len(self.specifier.subpredicates)
 
 		self._priority = priority
 		return priority
