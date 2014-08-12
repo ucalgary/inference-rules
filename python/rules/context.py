5f44b63756b92be8dc72e8a58c3fc1d014d5b720
King Chung Huang <kchuang@ucalgary.ca>
2014-08-12 16:35:28 -0600

    Modify Context to extend MutableMapping
    Change the superclass of Context from object to
    collections.MutableMapping, to support dict like behaviour in Context.
    Add stubs for required methods.

--- python/rules/context.py
+++ python/rules/context.py
@@ -1,12 +1,15 @@
 # coding=utf-8
 
+import collections
+
 from .model import DefaultModel
 
-class Context(object):
+class Context(collections.MutableMapping):
 
 	def __init__(self, model=None, parentContext=None):
 		self._model = parentContext.model if parentContext is not None else model if model is not None else DefaultModel
 		self._parentContext = parentContext
+		self._localValues = dict()
 
 	@property
 	def model(self):
@@ -18,3 +21,20 @@ class Context(object):
 	def inferAllPossibleValuesForKey(self, key):
 		self.model.fireAllRulesForKeyPathInContext(key, self)
 
+	def __getitem__(self, key):
+		pass
+
+	def __setitem__(self, key, value):
+		pass
+
+	def __delitem__(self, key):
+		pass
+
+	def __iter__(self):
+		pass
+
+	def __len__(self):
+		pass
+
+	def __keytransform__(self, key):
+		pass
